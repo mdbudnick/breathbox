5baf8ee6cd89e4c4f6f2dab8e10f9a6fa476f5de
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-08-23 11:32:40 -0400

    We have clockwise movement

--- script.js
+++ script.js
@@ -2,31 +2,38 @@ const box = document.querySelector('.breath-box');
 const circle = document.querySelector('.circle');
 
 function animateBreathing() {
-  const animationDuration = 4000; // in milliseconds
-  const inhaleDuration = 1000;
-  const exhaleDuration = 1000;
+  const inhaleDuration = 1;
+  const holdInDuration = 1;
+  const exhaleDuration = 1;
+  const holdOutDuration = 1;
+  let totalDuration = (inhaleDuration + holdInDuration + exhaleDuration + holdOutDuration) * 1000
 
-  circle.style.margin = `0 0 0 ${box.clientWidth}px`
-  circle.style.transition = `margin 4s`;
+  // Inhale (up)
+  circle.style.margin = `0 0 ${box.clientWidth}px 0`
+  circle.style.transition = `margin ${inhaleDuration}s`;
+  console.log('up')
 
+  // Hold In (right)
   setTimeout(() => {
-    circle.style.margin = `${box.clientHeight}px 0 0 ${box.clientHeight}px`
-    circle.style.transition = `margin 4s`;
-    
+    circle.style.margin = `0 0 0 ${box.clientWidth}px`
+    circle.style.transition = `margin ${holdInDuration}s`;
+    console.log('right')
+    // Exhale (down)
     setTimeout(() => {
-      circle.style.margin = `${box.clientHeight}px ${box.clientHeight}px 0 0`
-      circle.style.transition = `margin 4s`;
-
+      circle.style.margin = `${box.clientHeight}px 0 0 ${box.clientHeight}px`
+      circle.style.transition = `margin ${exhaleDuration}s`;
+      console.log('down')
+      // Hold out (left)
       setTimeout(() => {
-        circle.style.margin = `0 ${box.clientHeight}px ${box.clientHeight}px 0`
-        circle.style.transition = `margin 4s`;
-
+        circle.style.margin = `${box.clientHeight}px ${box.clientHeight}px 0 0`
+        circle.style.transition = `margin ${holdOutDuration}s`;
+        console.log('left')
         setTimeout(() => {
           animateBreathing(); // Restart the cycle
-        }, inhaleDuration);
-      }, exhaleDuration);
-    }, inhaleDuration);
-  }, exhaleDuration);
+        }, totalDuration);
+      }, totalDuration);
+    }, totalDuration);
+  }, totalDuration);
 }
 
 animateBreathing();
