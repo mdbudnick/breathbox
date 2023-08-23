0de649bbc5ceecc1eff846c9157c5d59601ec4c4
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-08-23 11:45:42 -0400

    Circular movement is more fluid

--- script.js
+++ script.js
@@ -6,34 +6,33 @@ function animateBreathing() {
   const holdInDuration = 1;
   const exhaleDuration = 1;
   const holdOutDuration = 1;
-  let totalDuration = (inhaleDuration + holdInDuration + exhaleDuration + holdOutDuration) * 1000
 
   // Inhale (up)
-  circle.style.margin = `0 0 ${box.clientWidth}px 0`
+  circle.style.margin = `0 0 ${box.clientHeight}px 0`
   circle.style.transition = `margin ${inhaleDuration}s`;
-  console.log('up')
 
   // Hold In (right)
   setTimeout(() => {
     circle.style.margin = `0 0 0 ${box.clientWidth}px`
     circle.style.transition = `margin ${holdInDuration}s`;
-    console.log('right')
+    
     // Exhale (down)
     setTimeout(() => {
-      circle.style.margin = `${box.clientHeight}px 0 0 ${box.clientHeight}px`
+      circle.style.margin = `${box.clientHeight}px 0 0 ${box.clientWidth}px`
       circle.style.transition = `margin ${exhaleDuration}s`;
-      console.log('down')
+      
       // Hold out (left)
       setTimeout(() => {
-        circle.style.margin = `${box.clientHeight}px ${box.clientHeight}px 0 0`
+        circle.style.margin = `${box.clientHeight}px ${box.clientWidth}px 0 0`
         circle.style.transition = `margin ${holdOutDuration}s`;
-        console.log('left')
+        
         setTimeout(() => {
           animateBreathing(); // Restart the cycle
-        }, totalDuration);
-      }, totalDuration);
-    }, totalDuration);
-  }, totalDuration);
+          // 675 gives smooth pathing
+        },  holdOutDuration * 675);
+      }, exhaleDuration * 675);
+    }, holdInDuration * 675);
+  }, inhaleDuration * 675);
 }
 
 animateBreathing();
