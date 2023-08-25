4128508939dc275a566264a48bc7d8b04981e757
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-08-25 12:58:42 -0400

    Use a transition-timing-function for breath and holding patterns

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const box = document.querySelector('.breath-box');
 const circle = document.querySelector('.circle');
 
 const SMOOTH_PATH_TIMING = 1000
-const BREATH_RATIO = 2
+const BREATH_RATIO = 5
+const BREATH_CURVE = "cubic-bezier(.13,.38,.48,1.02)"
 const HOLD_RATIO = 2
 
 
@@ -13,25 +14,25 @@ function animateBreathing() {
   const holdOutDuration = HOLD_RATIO;
 
   // Inhale (up)
-  circle.style.transition = `bottom ${inhaleDuration}s`;
+  circle.style.transition = `bottom ${inhaleDuration}s ${BREATH_CURVE}`;
   circle.style.bottom = `${box.clientHeight - circle.clientHeight/2}px`
   circle.style.left = `-${circle.clientWidth/2}px`
 
   // Hold In (right)
   setTimeout(() => {
-    circle.style.transition = `left ${holdInDuration}s`;
+    circle.style.transition = `left ${holdInDuration}s linear`;
     circle.style.bottom = `${box.clientHeight - circle.clientHeight/2}px`
     circle.style.left = `${box.clientWidth - (circle.clientWidth/2)}px`
     
     // Exhale (down)
     setTimeout(() => {
-      circle.style.transition = `bottom ${exhaleDuration}s`;
+      circle.style.transition = `bottom ${exhaleDuration}s  ${BREATH_CURVE}`;
       circle.style.bottom = `-${circle.clientHeight/2}px`
       circle.style.left = `${box.clientWidth - (circle.clientWidth/2)}px`
       
       // Hold out (left)
       setTimeout(() => {
-        circle.style.transition = `left ${holdOutDuration}s`;
+        circle.style.transition = `left ${holdOutDuration}s linear`;
         circle.style.bottom = `-${circle.clientHeight/2}px`
         circle.style.left = `-${circle.clientWidth/2}px`
         
