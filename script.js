10087b06f3ca3a508243743a78e466d1f5c124a3
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-08-25 13:06:35 -0400

    Add an action text to describe what is going on

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const box = document.querySelector('.breath-box');
 const circle = document.querySelector('.circle');
+const action = document.querySelector('.action');
 
 const SMOOTH_PATH_TIMING = 1000
 const BREATH_RATIO = 5
@@ -17,21 +18,25 @@ function animateBreathing() {
   circle.style.transition = `bottom ${inhaleDuration}s ${BREATH_CURVE}`;
   circle.style.bottom = `${box.clientHeight - circle.clientHeight/2}px`
   circle.style.left = `-${circle.clientWidth/2}px`
+  action.textContent = "INHALE"
 
   // Hold In (right)
   setTimeout(() => {
+    action.textContent = "HOLD"
     circle.style.transition = `left ${holdInDuration}s linear`;
     circle.style.bottom = `${box.clientHeight - circle.clientHeight/2}px`
     circle.style.left = `${box.clientWidth - (circle.clientWidth/2)}px`
     
     // Exhale (down)
     setTimeout(() => {
+      action.textContent = "EXHALE"
       circle.style.transition = `bottom ${exhaleDuration}s  ${BREATH_CURVE}`;
       circle.style.bottom = `-${circle.clientHeight/2}px`
       circle.style.left = `${box.clientWidth - (circle.clientWidth/2)}px`
       
       // Hold out (left)
       setTimeout(() => {
+        action.textContent = "HOLD"
         circle.style.transition = `left ${holdOutDuration}s linear`;
         circle.style.bottom = `-${circle.clientHeight/2}px`
         circle.style.left = `-${circle.clientWidth/2}px`
