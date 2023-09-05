ed77bd6b0f8145b987553a9bf6e4c56a3ef72520
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-05 18:05:20 -0400

    Don't need export not node

--- vh_func.js
+++ vh_func.js
@@ -1,17 +1,17 @@
-export function vh(percent) {
+function vh(percent) {
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     return (percent * h) / 100;
 }
   
-export function vw(percent) {
+function vw(percent) {
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     return (percent * w) / 100;
 }
 
-export function vmin(percent) {
+function vmin(percent) {
     return Math.min(vh(percent), vw(percent));
 }
 
-export function vmax(percent) {
+function vmax(percent) {
     return Math.max(vh(percent), vw(percent));
 }
