c8f75b157e5316e2794fbc4cb3a801498f931c19
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-06 20:19:55 -0400

    Add vh and vw to px functions

--- vh_func.js
+++ vh_func.js
@@ -8,6 +8,16 @@ function vwToPx(percent) {
     return (percent * w) / 100;
 }
 
+function pxToVh(px) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (px / h) * 100;
+}
+  
+function pxToVw(px) {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (px / w) * 100;
+}
+
 function vmin(percent) {
     return Math.min(vh(percent), vw(percent));
 }
