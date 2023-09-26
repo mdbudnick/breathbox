fe76baa0eb1fbcd2ccbe55bc9f65b5b67ad5d6b0
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-26 19:23:36 -0400

    Breakout startAndTimer.ts with funcitons related to start button and timer

--- www/js/breathbox.js
+++ www/js/breathbox.js
@@ -51,30 +51,6 @@ function animateBreathing() {
         }, holdInDuration * SMOOTH_PATH_TIMING);
     }, inhaleDuration * SMOOTH_PATH_TIMING);
 }
-let minutes = 0;
-let seconds = 0;
-let timerInterval;
-function startTimer() {
-    incrementTimer();
-    start.style.backgroundColor = "transparent";
-    start.style.border = "none";
-    start.classList.remove('button');
-    timerInterval = setInterval(incrementTimer, 1000);
-}
-function incrementTimer() {
-    ++seconds;
-    if (seconds == 60) {
-        ++minutes;
-        seconds = 0;
-    }
-    start.textContent = "" + minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
-}
-function addPauseButton() {
-    pauseButton.style.display = "flex";
-}
-function addStopButton() {
-    stopButton.style.display = "flex";
-}
 let started = false;
 function startBreathBox() {
     if (started) {
---
+++ www/js/startAndTimer.d.ts
@@ -0,0 +1,7 @@
+declare let minutes: number;
+declare let seconds: number;
+declare let timerInterval: number;
+declare function startTimer(): void;
+declare function incrementTimer(): void;
+declare function addPauseButton(): void;
+declare function addStopButton(): void;
