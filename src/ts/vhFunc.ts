94b69de8df607a888f7c139cd8c973a6d087113d
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-27 18:45:15 -0400

    Export functions from src/ts/vhFunc.ts

--- src/ts/vhFunc.ts
+++ src/ts/vhFunc.ts
@@ -1,27 +1,27 @@
-function vhToPx(percent: number): number {
+export function vhToPx(percent: number): number {
     const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     return (percent * h) / 100;
 }
 
-function vwToPx(percent: number): number {
+export function vwToPx(percent: number): number {
     const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     return (percent * w) / 100;
 }
 
-function pxToVh(px: number): number {
+export function pxToVh(px: number): number {
     const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     return (px / h) * 100;
 }
 
-function pxToVw(px: number): number {
+export function pxToVw(px: number): number {
     const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     return (px / w) * 100;
 }
 
-function vmin(percent: number): number {
+export function vmin(percent: number): number {
     return Math.min(pxToVh(percent), pxToVw(percent));
 }
 
-function vmax(percent: number): number {
+export function vmax(percent: number): number {
     return Math.max(pxToVh(percent), pxToVw(percent));
 }
