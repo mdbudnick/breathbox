07011c2ae5150e9140c28583d9ef07f82503c96b
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-28 19:54:01 -0400

    terraform apply working

--- main.tf
+++ main.tf
@@ -4,9 +4,11 @@ module "static-website-cdn" {
   location = "East US"
   create_resource_group = true
   resource_group_name = "breathbox"
-  storage_account_name = "breathboxstorage"
+  storage_account_name = "sabreathbox"
   static_website_source_folder = "src"
   enable_cdn_profile = true
+  cdn_sku_profile = "Standard_Microsoft"
+  cdn_profile_name = "breathboxcdn"
   friendly_name = "breathbox"
   allowed_origins = ["breathbox.net"]
   tags = { app: "breathbox" }
