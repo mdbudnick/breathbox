d895da385ccc28e4d2e02f0e84e4a5fd98038948
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-28 18:03:50 -0400

    Delete old vh_func files

--- src/js/vh_func.d.ts
+++
@@ -1,6 +0,0 @@
-declare function vhToPx(percent: number): number;
-declare function vwToPx(percent: number): number;
-declare function pxToVh(px: number): number;
-declare function pxToVw(px: number): number;
-declare function vmin(percent: number): number;
-declare function vmax(percent: number): number;
