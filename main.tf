d1070b0a344c021589018ed37812ef7b629f03d4
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-28 18:17:12 -0400

    terraform plan working

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 module "static-website-cdn" {
   source  = "kumarvna/static-website-cdn/azurerm"
   version = "2.2.0"
+  location = "East US"
   create_resource_group = true
   resource_group_name = "breathbox"
-  storage_account_name = "breathbox-static"
+  storage_account_name = "breathboxstorage"
   static_website_source_folder = "src"
   enable_cdn_profile = true
-  allowed_origins = null
+  friendly_name = "breathbox"
+  allowed_origins = ["breathbox.net"]
   tags = { app: "breathbox" }
 }
