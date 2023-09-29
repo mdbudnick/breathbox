4cd62f697ff72e391095bb87fc4d0fe0e705d344
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-09-28 20:35:13 -0400

    Use new common.ts and timer.ts in reset.ts

--- src/ts/reset.ts
+++ src/ts/reset.ts
@@ -1,56 +1,52 @@
+import * as common from './common'
+import { SharedIntervals } from './sharedIntervals'
+import { Timer } from './timer'
+
 function resetCircle() {
-  circle.style.width = SMALL_CIRCLE_SIZE + "vh";
-  circle.style.height = SMALL_CIRCLE_SIZE + "vh";
-  circle.style.backgroundColor = RESET_ORANGE;
-  circle.style.transitionProperty = "";
-  circle.style.transitionDuration = "";
-  circle.style.transitionTimingFunction = "";
-  circle.style.bottom = "-1vh";
-  circle.style.left = "-1vh";
+  common.circle.style.width = common.SMALL_CIRCLE_SIZE + "vh";
+  common.circle.style.height = common.SMALL_CIRCLE_SIZE + "vh";
+  common.circle.style.backgroundColor = common.RESET_ORANGE;
+  common.circle.style.transitionProperty = "";
+  common.circle.style.transitionDuration = "";
+  common.circle.style.transitionTimingFunction = "";
+  common.circle.style.bottom = "-1vh";
+  common.circle.style.left = "-1vh";
 }
 
 function resetStartButton() {
-  start.style.color = "white";
-  start.style.border = "4px solid green";
-  start.style.backgroundColor = "lightgreen";
-  start.style.borderRadius = "5vw";
-  start.textContent = "Start";
-  start.classList.add("button");
+  common.start.style.color = "white";
+  common.start.style.border = "4px solid green";
+  common.start.style.backgroundColor = "lightgreen";
+  common.start.style.borderRadius = "5vw";
+  common.start.textContent = "Start";
+  common.start.classList.add("button");
 }
 
 function resetActionText(text: string) {
-  text = text || DEFAULT_ACTION_TEXT;
-  action.textContent = text;
-  action.style.fontSize = DEFAULT_ACTION_FONT_SIZE;
-  action.style.color = RESET_ORANGE;
+  text = text || common.DEFAULT_ACTION_TEXT;
+  common.action.textContent = text;
+  common.action.style.fontSize = common.DEFAULT_ACTION_FONT_SIZE;
+  common.action.style.color = common.RESET_ORANGE;
 }
 resetActionText("");
 
-let inhaleAnimation: ReturnType<typeof setTimeout> | null;
-let inhaleCountdownInterval: ReturnType<typeof setInterval> | null;
-let holdInAnimation: ReturnType<typeof setTimeout> | null;
-let holdInCountdownInterval: ReturnType<typeof setInterval> | null;
-let exhaleAnimation: ReturnType<typeof setTimeout> | null;
-let exhaleCountdownInterval: ReturnType<typeof setInterval> | null;
-let holdOutAnimation:  ReturnType<typeof setTimeout> | null;
-let holdOutCountdownInterval: ReturnType<typeof setInterval> | null;
 function resetAnimations() {
-  clearTimeout(inhaleAnimation!);
-  inhaleAnimation = null;
-  clearInterval(inhaleCountdownInterval!);
-  inhaleCountdownInterval = null;
-  clearTimeout(holdInAnimation!);
-  holdInAnimation = null;
-  clearInterval(holdInCountdownInterval!);
-  holdInCountdownInterval = null;
-  clearTimeout(exhaleAnimation!);
-  exhaleAnimation = null;
-  clearInterval(exhaleCountdownInterval!);
-  exhaleCountdownInterval = null;
-  clearTimeout(holdOutAnimation!);
-  holdOutAnimation = null;
-  clearInterval(holdOutCountdownInterval!);
-  holdOutCountdownInterval = null;
-  clearInterval(timerInterval!);
-  timerInterval = null;
+  clearTimeout(SharedIntervals.inhaleAnimation!);
+  SharedIntervals.inhaleAnimation = null;
+  clearInterval(SharedIntervals.inhaleCountdownInterval!);
+  SharedIntervals.inhaleCountdownInterval = null;
+  clearTimeout(SharedIntervals.holdInAnimation!);
+  SharedIntervals.holdInAnimation = null;
+  clearInterval(SharedIntervals.holdInCountdownInterval!);
+  SharedIntervals.holdInCountdownInterval = null;
+  clearTimeout(SharedIntervals.exhaleAnimation!);
+  SharedIntervals.exhaleAnimation = null;
+  clearInterval(SharedIntervals.exhaleCountdownInterval!);
+  SharedIntervals.exhaleCountdownInterval = null;
+  clearTimeout(SharedIntervals.holdOutAnimation!);
+  SharedIntervals.holdOutAnimation = null;
+  clearInterval(SharedIntervals.holdOutCountdownInterval!);
+  SharedIntervals.holdOutCountdownInterval = null;
+  clearInterval(Timer.timerInterval!);
+  Timer.timerInterval = null;
 }
