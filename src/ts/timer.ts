aef8de4b84a0d1520ee41f752b10b7c29a98d1e7
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-10-11 19:24:40 -0400

    Format timer.ts

--- src/ts/timer.ts
+++ src/ts/timer.ts
@@ -1,4 +1,11 @@
-import { pauseButton, start, stopButton, timerMinutesInput, timerSecondsInput, timerDirection } from "./common";
+import {
+  pauseButton,
+  start,
+  stopButton,
+  timerMinutesInput,
+  timerSecondsInput,
+  timerDirection,
+} from "./common";
 
 class TimerClass {
   minutes: number;
@@ -10,19 +17,23 @@ class TimerClass {
     this.minutes = 0;
     this.seconds = 0;
     this.targetTime = 600;
-    this.timerInterval = null
+    this.timerInterval = null;
   }
 
   startTimer() {
-    let timerFn = timerDirection.classList.contains("point-up") ? this.incrementTimer : this.decrementTimer;
+    let timerFn = timerDirection.classList.contains("point-up")
+      ? this.incrementTimer
+      : this.decrementTimer;
     timerFn.bind(this)();
-    this.targetTime = (parseInt(timerMinutesInput.value) * 60) + parseInt(timerSecondsInput.value)
+    this.targetTime =
+      parseInt(timerMinutesInput.value) * 60 +
+      parseInt(timerSecondsInput.value);
     start.style.backgroundColor = "transparent";
     start.style.border = "none";
     start.classList.remove("button");
     this.timerInterval = setInterval(timerFn.bind(this), 1000);
   }
-  
+
   incrementTimer() {
     ++this.seconds;
     if (this.seconds == 60) {
@@ -30,7 +41,10 @@ class TimerClass {
       this.seconds = 0;
     }
     start.textContent =
-      "" + this.minutes + ":" + (this.seconds < 10 ? "0" + this.seconds : this.seconds);
+      "" +
+      this.minutes +
+      ":" +
+      (this.seconds < 10 ? "0" + this.seconds : this.seconds);
   }
 
   decrementTimer() {
@@ -40,24 +54,31 @@ class TimerClass {
       this.seconds = 60;
     }
     start.textContent =
-      "" + this.minutes + ":" + (this.seconds < 10 ? "0" + this.seconds : this.seconds);
+      "" +
+      this.minutes +
+      ":" +
+      (this.seconds < 10 ? "0" + this.seconds : this.seconds);
   }
-  
+
   addPauseButton() {
     pauseButton.style.display = "flex";
   }
-  
+
   addStopButton() {
     stopButton.style.display = "flex";
   }
 
   reset() {
-    this.minutes = timerDirection.classList.contains("point-up") ? 0 : parseInt(timerMinutesInput.value);
-    this.seconds = timerDirection.classList.contains("point-up") ? 0 : parseInt(timerSecondsInput.value);
+    this.minutes = timerDirection.classList.contains("point-up")
+      ? 0
+      : parseInt(timerMinutesInput.value);
+    this.seconds = timerDirection.classList.contains("point-up")
+      ? 0
+      : parseInt(timerSecondsInput.value);
   }
 
   reachedTime() {
-    return (this.minutes * 60) + this.seconds >= this.targetTime;
+    return this.minutes * 60 + this.seconds >= this.targetTime;
   }
 }
 
