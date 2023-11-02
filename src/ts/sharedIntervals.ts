cf1d0d99f3e54f4ce9f34337d08cda4272b8d4c5
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-11-02 19:46:14 -0400

    Add new setters in sharedIntervals for safety

--- src/ts/sharedIntervals.ts
+++ src/ts/sharedIntervals.ts
@@ -12,11 +12,76 @@ class Timeouts {
 
   inhaleAnimation: ReturnType<typeof setTimeout> | null
   inhaleCountdownInterval: ReturnType<typeof setInterval> | null
-  holdInAnimation: ReturnType<typeof setTimeout> | null
-  holdInCountdownInterval: ReturnType<typeof setInterval> | null
   exhaleAnimation: ReturnType<typeof setTimeout> | null
   exhaleCountdownInterval: ReturnType<typeof setInterval> | null
+  holdInAnimation: ReturnType<typeof setTimeout> | null
+  holdInCountdownInterval: ReturnType<typeof setInterval> | null
   holdOutAnimation: ReturnType<typeof setTimeout> | null
   holdOutCountdownInterval: ReturnType<typeof setInterval> | null
+
+  setInhaleAnimation (timer: ReturnType<typeof setTimeout>): boolean {
+    if (this.inhaleAnimation !== null) {
+      clearTimeout(this.inhaleAnimation)
+    }
+    this.inhaleAnimation = timer
+    return true
+  }
+
+  setInhaleCountdownInterval (interval: ReturnType<typeof setInterval>): boolean {
+    if (this.inhaleCountdownInterval !== null) {
+      clearInterval(this.inhaleCountdownInterval)
+    }
+    this.inhaleCountdownInterval = interval
+    return true
+  }
+
+  setHoldInAnimation (timer: ReturnType<typeof setTimeout>): boolean {
+    if (this.holdInAnimation !== null) {
+      clearTimeout(this.holdInAnimation)
+    }
+    this.holdInAnimation = timer
+    return true
+  }
+
+  setHoldInCountdownInterval (interval: ReturnType<typeof setInterval>): boolean {
+    if (this.holdInCountdownInterval !== null) {
+      clearInterval(this.holdInCountdownInterval)
+    }
+    this.holdInCountdownInterval = interval
+    return true
+  }
+
+  setHoldOutAnimation (timer: ReturnType<typeof setTimeout>): boolean {
+    if (this.holdOutAnimation !== null) {
+      clearTimeout(this.holdOutAnimation)
+    }
+    this.holdOutAnimation = timer
+    return true
+  }
+
+  setHoldOutCountdownInterval (interval: ReturnType<typeof setInterval>): boolean {
+    if (this.holdOutCountdownInterval !== null) {
+      clearInterval(this.holdOutCountdownInterval)
+    }
+    this.holdOutCountdownInterval = interval
+    return true
+  }
+
+  setExhaleAnimation (timer: ReturnType<typeof setTimeout>): boolean {
+    if (this.exhaleAnimation !== null) {
+      clearTimeout(this.exhaleAnimation)
+    }
+    this.exhaleAnimation = timer
+    return true
+  }
+
+  setExhaleCountdownInterval (interval: ReturnType<typeof setInterval>): boolean {
+    if (this.exhaleCountdownInterval !== null) {
+      clearInterval(this.exhaleCountdownInterval)
+    }
+    this.exhaleCountdownInterval = interval
+    return true
+  }
 }
+
 export const SharedIntervals = new Timeouts()
