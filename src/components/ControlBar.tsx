24321636b8f98e47391a0d0c0a0f871a609e72f5
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-11-02 20:25:20 -0400

    Pass startAnimation to ControlBar component

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -1,11 +1,15 @@
-import React, { type FC, type PropsWithChildren } from 'react'
+import React, { type FC } from 'react'
 import '../css/control-bar.css'
 import '../img/play-pause.svg'
 
-const ControlBar: FC = (prop: PropsWithChildren) => {
+interface ControlBarProps {
+  animateFn: () => void
+}
+
+const ControlBar: FC<ControlBarProps> = (props) => {
   return (
     <div className="control-bar">
-      <div className="timer-start button">Start</div>
+      <div className="timer-start button" onClick={props.animateFn}>Start</div>
       <img className="pause button" src="static/media/play-pause.svg"></img>
       <div className="stop button"></div>
     </div>
