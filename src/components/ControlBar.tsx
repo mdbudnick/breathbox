1d7d5bd6c81b67059e021f4c5efc41121c2fa4e4
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-11-02 20:37:06 -0400

    Update ControlBarProps

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -3,13 +3,12 @@ import '../css/control-bar.css'
 import '../img/play-pause.svg'
 
 interface ControlBarProps {
-  animateFn: () => void
 }
 
 const ControlBar: FC<ControlBarProps> = (props) => {
   return (
     <div className="control-bar">
-      <div className="timer-start button" onClick={props.animateFn}>Start</div>
+      <div className="timer-start button">Start</div>
       <img className="pause button" src="static/media/play-pause.svg"></img>
       <div className="stop button"></div>
     </div>
