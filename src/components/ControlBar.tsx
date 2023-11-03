ebb2267a89d663d287386f4b0d11b7314b3babfc
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-11-02 21:16:58 -0400

    Add useState<StartButtonStyle> to ControlBar component with default style

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -1,4 +1,4 @@
-import React, { type FC } from 'react'
+import React, { useState, type FC } from 'react'
 import '../css/control-bar.css'
 import '../img/play-pause.svg'
 
@@ -8,19 +8,32 @@ interface ControlBarProps {
   stopFn: () => void
 }
 
-export function resetStartButton(): undefined {
-  common.start.style.color = 'white'
-  common.start.style.border = '4px solid green'
-  common.start.style.backgroundColor = 'lightgreen'
-  common.start.style.borderRadius = '5vw'
-  common.start.textContent = 'Start'
-  common.start.classList.add('button')
+interface StartButtonStyle {
+  color: string
+  border: string
+  backgroundColor: string
+  borderRadius: string
+}
+
+const START_BUTTON_RESET_STYLE: StartButtonStyle = {
+  color: 'white',
+  border: '4px solid green',
+  backgroundColor: 'lightgreen',
+  borderRadius: '5vw'
 }
 
 const ControlBar: FC<ControlBarProps> = (props) => {
+  const [startButtonStyle, setStartButtonStyle] = useState<StartButtonStyle>(START_BUTTON_RESET_STYLE)
+
+  function resetStartButton (): undefined {
+    setStartButtonStyle(START_BUTTON_RESET_STYLE)
+    common.start.textContent = 'Start'
+    common.start.classList.add('button')
+  }
+
   return (
     <div className={props.started ? 'control-bar top-buffer' : 'control-bar'}>
-      <div className="timer-start button" onClick={props.startFn}>Start</div>
+      <div className="timer-start button" style={startButtonStyle} onClick={props.startFn}>Start</div>
       <img className="pause button" src="static/media/play-pause.svg" style={{ display: props.started ? 'none' : 'flex' }}></img>
       <div className="stop button" style={{ display: props.started ? 'none' : 'flex' }} onClick={props.stopFn}></div>
     </div>
