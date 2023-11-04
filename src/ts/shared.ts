790f8c31c0c1a8cb0372eb3fb98a212ac4c75843
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-11-04 10:07:43 -0400

    Remove unused ts files and lines

--- src/ts/shared.ts
+++ src/ts/shared.ts
@@ -1,27 +1,3 @@
-export const box = document.querySelector('.breath-box') as HTMLElement
-export const boxRect = box.getBoundingClientRect()
-export const start = document.querySelector('.timer-start') as HTMLElement
-export const stopButton = document.querySelector('.stop') as HTMLElement
-export const pauseButton = document.querySelector('.pause') as HTMLElement
-export const breathTimeInput = document.querySelector(
-  '#breath-time'
-) as HTMLInputElement
-export const holdTimeInput = document.querySelector(
-  '#hold-time'
-) as HTMLInputElement
-export const timerMinutesInput = document.querySelector(
-  '#countdown-minutes'
-) as HTMLInputElement
-export const timerSecondsInput = document.querySelector(
-  '#countdown-seconds'
-) as HTMLInputElement
-export const timerDirection = document.querySelector(
-  '#time-arrow'
-) as HTMLElement
-export const config = document.querySelector('.config') as HTMLElement
-export const controlBar = document.querySelector('.control-bar') as HTMLElement
-
-export const DEFAULT_BACKGROUND_COLOR = '#1e3250'
 export const INHALE_COLOR = '#0f5362'
 export const EXHALE_COLOR = '#c08845'
 export const RESET_ORANGE = '#f6786e'
@@ -32,9 +8,6 @@ export const SMALL_CIRCLE_SIZE = 2
 export const SMOOTH_PATH_TIMING = 1000
 export const BREATH_CURVE = 'cubic-bezier(.13,.38,.48,1.02)'
 
-export const INHALE = 'INHALE'
-export const EXHALE = 'EXHALE'
-export const HOLD = 'HOLD'
 export const INHALE_SIZE = 8
 export const EXHALE_SIZE = 4
 export const DEFAULT_ACTION_TEXT = 'Breath Box'
