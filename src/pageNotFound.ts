f29ef0baa016f27dd66ed9a5fec6831c4f73e512
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-02 18:52:36 -0500

    Remove Page Not Found components because it would require unnecessary routing with react-native

--- src/pageNotFound.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import PageNotFound from './components/PageNotFound'
-
-ReactDOM.render(
-  React.createElement(PageNotFound),
-  document.getElementById('app')
-)
