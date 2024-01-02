84d60adc730b7bf2051d570944aea6f05e5d8392
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-02 18:53:28 -0500

    Initialize shared stylesheet.ts

---
+++ src/styles/stylesheet.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  linkText: {
+    color: 'blue',
+    textDecorationLine: 'underline'
+  }
+})
+
+export default commonStyles
