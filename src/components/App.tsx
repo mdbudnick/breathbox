09d3a120df09d20065578b194fa65b2610180059
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-03 18:29:15 -0500

    Convert central style to StyleSheets in App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,23 @@
 import React, { type FC, type PropsWithChildren } from 'react'
 import BreathBox from './BreathBox'
 import '../img/buddha-gnome.jpg'
-import '../css/central.css'
+import { StyleSheet, View } from 'react-native'
+import { screenWidth } from '../ts/windowDimensions'
+
+const styles = StyleSheet.create({
+  central: {
+    marginTop: '10%',
+    alignSelf: 'center',
+    width: '30%',
+    height: '80%',
+    fontFamily: 'Haettenschweiler, "Arial Narrow Bold", sans-serif'
+  },
+  centralResponsive: {
+    marginTop: '15%',
+    width: '80%',
+    height: '70%'
+  }
+})
 
 const App: FC = (prop: PropsWithChildren) => {
   return (
