2a9002db97416c7a67821b384c09f46ad6288b9d
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-05 12:14:47 -0500

    Initial animation sequence (testing) for Circle component

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,5 +1,6 @@
-import React, { type FC, useEffect, useState } from 'react'
+import React, { type FC, useEffect, useRef, useState } from 'react'
 import {
+    Animated,
     StyleSheet,
     View
 } from 'react-native'
@@ -21,15 +22,64 @@ const styles = StyleSheet.create({
 })
 
 const Circle: FC<CircleProps> = (props) => {
+    const circleBottom = useRef(new Animated.Value(props.boundingHeight)).current
+    const circleLeft = useRef(new Animated.Value(0)).current
+
     const [circleStyle, setCircleStyle] = useState({
         width: SMALL_CIRCLE_SIZE,
         height: SMALL_CIRCLE_SIZE,
         backgroundColor: 'rgb(245, 121, 112)',
         borderRadius: 50,
         transformOrigin: 'center',
-        bottom: -1,
-        left: -1
+        bottom: props.boundingHeight,
+        left: props.boundingWidth
     });
+
+    const toBottomLeft = Animated.parallel([
+        Animated.timing(circleBottom, {
+            toValue: props.boundingHeight,
+            duration: 1000,
+            useNativeDriver: false,
+        }),
+        Animated.timing(circleLeft, {
+            toValue: -(SMALL_CIRCLE_SIZE / 2),
+            duration: 1000,
+            useNativeDriver: false,
+        })
+    ])
+    const toTopLeft = Animated.parallel([
+        Animated.timing(circleBottom, {
+            toValue: 10,
+            duration: 1000,
+            useNativeDriver: false,
+        }),
+        Animated.timing(circleLeft, {
+            toValue: -(SMALL_CIRCLE_SIZE / 2),
+            duration: 1000,
+            useNativeDriver: false,
+        })
+    ])
+
+    const clockWiseAnimation = Animated.sequence([
+        toBottomLeft,
+        toTopLeft
+    ])
+
+    useEffect(() => {
+        if (props.running) {
+            clockWiseAnimation.start()
+        } else {
+            clockWiseAnimation.reset()
+        }
+    }, [props.running])
+
+    useEffect(() => {
+        setCircleStyle({
+          ...circleStyle,
+          bottom: parseInt(JSON.stringify(circleBottom)), 
+          left: parseInt(JSON.stringify(circleLeft)),
+        });
+      }, [circleBottom, circleLeft])
 
     return <View style={[{ position: 'absolute' }, circleStyle]} />;
 }
