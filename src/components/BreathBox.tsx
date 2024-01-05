6493a4e1a646e265a38d52623a7b45d87eed62d5
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-05 13:00:30 -0500

    Migration out of BreathBox to Circle and ActionText components

--- src/components/BreathBox.tsx
+++ src/components/BreathBox.tsx
@@ -5,25 +5,8 @@ import ControlBar from './ControlBar'
 import Config from './Config'
 import Congrats from './Congratulations'
 import { SharedIntervals } from '../ts/sharedIntervals'
-import {
-  type ConfigSetters,
-  type ConfigInput
-} from '../ts/shared'
+import { type ConfigSetters, type ConfigInput } from '../ts/shared'
 import { StyleSheet, View } from 'react-native'
-
-const INHALE_COLOR = '#0f5362'
-const EXHALE_COLOR = '#c08845'
-const RESET_ORANGE = '#f6786e'
-
-const LARGE_CIRCLE_SIZE = 6
-const SMALL_CIRCLE_SIZE = 2
-
-const SMOOTH_PATH_TIMING = 1000
-const BREATH_CURVE = 'cubic-bezier(.13,.38,.48,1.02)'
-
-const INHALE_SIZE = 8
-const EXHALE_SIZE = 4
-const DEFAULT_ACTION_FONT_SIZE = '5vh'
 
 const styles = StyleSheet.create({
   breathBox: {
