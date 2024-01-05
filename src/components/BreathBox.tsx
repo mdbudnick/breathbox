3dc8b92febc32dae4f4120865156b999a81b2839
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-04 19:38:47 -0500

    Start breaking out pieces for ActionText and Circle to new components

--- src/components/BreathBox.tsx
+++ src/components/BreathBox.tsx
@@ -1,15 +1,15 @@
-import React, { useRef, useState, type FC, type PropsWithChildren } from 'react'
+import React, { useState, type FC, type PropsWithChildren } from 'react'
+import ActionText from './ActionText'
+import Circle from './Circle'
 import ControlBar from './ControlBar'
 import Config from './Config'
 import Congrats from './Congratulations'
 import { SharedIntervals } from '../ts/sharedIntervals'
-import { vhToPx } from '../ts/vhFunc'
 import {
-  type ActionStyle,
   type ConfigSetters,
   type ConfigInput
 } from '../ts/shared'
-import { StyleSheet, Text, View, type ViewStyle } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 const INHALE_COLOR = '#0f5362'
 const EXHALE_COLOR = '#c08845'
@@ -43,95 +43,10 @@ const styles = StyleSheet.create({
     // backgroundImage: require('../img/buddha-gnome.jpg'),
     resizeMode: 'cover',
     justifyContent: 'center'
-  },
-  circle: {
-    position: 'absolute',
-    width: '2%',
-    height: '2%',
-    backgroundColor: 'rgb(245, 121, 112)',
-    borderRadius: 50,
-    transformOrigin: 'center',
-    bottom: '-1%',
-    left: '-1%'
-  },
-  action: {
-    fontWeight: 'bold',
-    textAlign: 'center',
-    height: '10%',
-    borderRadius: 3,
-    opacity: 0.9,
-    padding: '1%', // Adjust as needed
-    backgroundColor: 'rgba(255, 255, 255, 0.1)',
-    textShadowColor: 'black',
-    textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 0.5
   }
 })
 
 const BreathBox: FC = (prop: PropsWithChildren) => {
-  const [action, setActionText] = useState<string>('Breath Box')
-  const [actionStyle, setActionStyle] = useState<ActionStyle>({
-    transitionDuration: '',
-    transitionTimingFunction: `${BREATH_CURVE}`,
-    fontSize: '5vh',
-    color: RESET_ORANGE
-  })
-
-  function resetActionText (): void {
-    setActionText('Breath Box')
-    setActionStyle({
-      ...actionStyle,
-      fontSize: DEFAULT_ACTION_FONT_SIZE,
-      color: RESET_ORANGE
-    })
-  }
-
-  const [circleStyle, setCircleStyle] = useState<ViewStyle>({
-    transitionProperty: '',
-    transitionDuration: '',
-    transitionTimingFunction: BREATH_CURVE,
-    backgroundColor: 'rgb(245, 121, 112)',
-    height: `${SMALL_CIRCLE_SIZE}%`,
-    width: `${SMALL_CIRCLE_SIZE}%`,
-    bottom: '-1%',
-    left: '-1%'
-  })
-
-  function resetCircleStyle (): void {
-    setCircleStyle({
-      transitionProperty: '',
-      transitionDuration: '',
-      transitionTimingFunction: BREATH_CURVE,
-      backgroundColor: INHALE_COLOR,
-      height: `${SMALL_CIRCLE_SIZE}vh`,
-      width: `${SMALL_CIRCLE_SIZE}vh`,
-      bottom: '-1vh',
-      left: '-1vh'
-    })
-  }
-
-  const startCountdownDecrement = (
-    text: string,
-    time: number
-  ): ReturnType<typeof setInterval> => {
-    let countdownInterval: ReturnType<typeof setInterval> | null
-    countdownInterval = setInterval(() => {
-      --time
-      if (time !== 0) {
-        setActionText(text + '\r\n' + time)
-      } else {
-        setActionText(text)
-        // It cancels itself
-        clearInterval(countdownInterval!)
-        countdownInterval = null
-      }
-    }, 1000)
-    // Do it the first time
-    setActionText(text + '\r\n' + time)
-
-    return countdownInterval
-  }
-
   const [started, setStarted] = useState<boolean>(false)
   const [paused, setPaused] = useState<boolean>(false)
   const [timeReached, setTimeReached] = useState<boolean>(false)
@@ -161,99 +76,6 @@ const BreathBox: FC = (prop: PropsWithChildren) => {
     setInputMinutes,
     setInputSeconds,
     setCountDirection
-  }
-
-  const boxRef = useRef<HTMLDivElement>(null)
-  let boxSize = { width: 0, height: 0 }
-  function getBoxSize (): void {
-    if (boxRef.current != null) {
-      const { width, height } = boxRef.current.getBoundingClientRect()
-      boxSize = { width, height }
-    }
-  }
-
-  const animateBreathing = (): void => {
-    // Inhale (up)
-    SharedIntervals.setInhaleCountdownInterval(
-      startCountdownDecrement('Inhale', breathDuration)
-    )
-    setActionStyle({
-      ...actionStyle,
-      transitionDuration: `${breathDuration}s`,
-      fontSize: `${INHALE_SIZE}vh`,
-      color: INHALE_COLOR
-    })
-    getBoxSize()
-    setCircleStyle({
-      ...circleStyle,
-      backgroundColor: INHALE_COLOR,
-      height: `${LARGE_CIRCLE_SIZE}%`,
-      width: `${LARGE_CIRCLE_SIZE}%`,
-      bottom: boxSize.height - vhToPx(LARGE_CIRCLE_SIZE) / 2,
-      left: LARGE_CIRCLE_SIZE / 2
-    })
-
-    // Hold In (right)
-    SharedIntervals.setHoldInAnimation(
-      setTimeout(() => {
-        SharedIntervals.setHoldInCountdownInterval(
-          startCountdownDecrement('Hold', holdDuration)
-        )
-        getBoxSize()
-        setCircleStyle({
-          ...circleStyle,
-          bottom: boxSize.height - vhToPx(SMALL_CIRCLE_SIZE) / 2,
-          left: boxSize.width - vhToPx(SMALL_CIRCLE_SIZE) / 2
-        })
-
-        // Exhale (down)
-        SharedIntervals.setExhaleAnimation(
-          setTimeout(() => {
-            SharedIntervals.setExhaleCountdownInterval(
-              startCountdownDecrement('Exhale', breathDuration)
-            )
-
-            setActionStyle({
-              ...actionStyle,
-              transitionDuration: `${breathDuration}s`,
-              fontSize: `${EXHALE_SIZE}vh`,
-              color: EXHALE_COLOR
-            })
-            getBoxSize()
-            setCircleStyle({
-              ...circleStyle,
-              backgroundColor: EXHALE_COLOR,
-              height: `${SMALL_CIRCLE_SIZE}%`,
-              width: `${SMALL_CIRCLE_SIZE}%`,
-              bottom: -SMALL_CIRCLE_SIZE / 2,
-              left: boxSize.width - vhToPx(SMALL_CIRCLE_SIZE) / 2
-            })
-
-            // Hold out (left)
-            SharedIntervals.setHoldOutAnimation(
-              setTimeout(() => {
-                SharedIntervals.setHoldOutCountdownInterval(
-                  startCountdownDecrement('Hold', holdDuration)
-                )
-                setCircleStyle({
-                  ...circleStyle,
-                  transitionDuration: `${holdDuration}s`,
-                  transitionTimingFunction: 'linear',
-                  bottom: `-${SMALL_CIRCLE_SIZE / 2}vh`,
-                  left: `-${SMALL_CIRCLE_SIZE / 2}vh`
-                })
-
-                SharedIntervals.setInhaleAnimation(
-                  setTimeout(() => {
-                    animateBreathing() // Restart the cycle
-                  }, holdDuration * SMOOTH_PATH_TIMING)
-                )
-              }, breathDuration * SMOOTH_PATH_TIMING)
-            )
-          }, holdDuration * SMOOTH_PATH_TIMING)
-        )
-      }, breathDuration * SMOOTH_PATH_TIMING)
-    )
   }
 
   function validInputs (): boolean {
@@ -288,25 +110,17 @@ const BreathBox: FC = (prop: PropsWithChildren) => {
     }
     setStarted(true)
     setPaused(false)
-    resetActionText()
-    resetCircleStyle()
-    animateBreathing()
   }
 
   function stopBreathBox (): void {
     setStarted(false)
     setPaused(false)
-    SharedIntervals.resetAnimations()
-    resetActionText()
-    resetCircleStyle()
   }
 
   function pauseBreathBox (): void {
     SharedIntervals.resetAnimations()
     setPaused(true)
     setActionText('Paused')
-    setActionStyle({ ...actionStyle, color: '#ff8c00' })
-    resetCircleStyle()
   }
 
   const ControlBarComponent = (
@@ -322,8 +136,6 @@ const BreathBox: FC = (prop: PropsWithChildren) => {
       pauseFn={pauseBreathBox}
       actionStyle={actionStyle}
       setActionStyle={setActionStyle}
-      setActionText={setActionText}
-      resetCircleStyle={resetCircleStyle}
       configInput={configInput}
     />
   )
@@ -352,9 +164,9 @@ const BreathBox: FC = (prop: PropsWithChildren) => {
           )}
       <View style={styles.breathBoxInner}>
         {started ? ControlBarComponent : [ControlBarComponent, ConfigComponent]}
-        <Text style={styles.action}>{action}</Text>
+        <ActionText></ActionText>
       </View>
-      <View style={[styles.circle, circleStyle]}></View>
+      <Circle></Circle>
     </View>
   )
 }
