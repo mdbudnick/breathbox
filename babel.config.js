87714cc8101151e6e834915c01059b78d0ffb8c7
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-06 17:02:57 -0500

    Merge expo configuration with outer project

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,36 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false
-      }
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false
+        }
+      ],
+      'babel-preset-expo',
+      '@babel/preset-react'
     ],
-    '@babel/preset-react'
-  ],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-transform-typescript',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties'
-  ],
-  env: {
-    production: {
-      only: ['src'],
-      plugins: [
-        [
-          'transform-react-remove-prop-types',
-          {
-            removeImport: true
-          }
-        ],
-        '@babel/plugin-transform-react-inline-elements',
-        '@babel/plugin-transform-react-constant-elements'
-      ]
+    plugins: [
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-transform-typescript',
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-class-properties'
+    ],
+    env: {
+      production: {
+        only: ['src'],
+        plugins: [
+          [
+            'transform-react-remove-prop-types',
+            {
+              removeImport: true
+            }
+          ],
+          '@babel/plugin-transform-react-inline-elements',
+          '@babel/plugin-transform-react-constant-elements'
+        ]
+      }
     }
   }
 }
